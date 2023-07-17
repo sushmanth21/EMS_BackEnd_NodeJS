03160ba6582c34c0029538d513fd14c23f7d9595
sushmanth21 <sushmanth21@gmail.com>
2023-07-17 17:29:04 +0530

    second commit

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -4,52 +4,89 @@ let EmployeeController = {};
 
 //--------- Get list of all employees ---------
 EmployeeController.getEmployees = async (req, res, next)=> {
-    let allEmployees =await Employee.find()
+    let allEmployees;
 
+    try{
+        allEmployees = await Employee.find();
+
+        if(!allEmployees){
+            return res.status(404).json({message: "No Employees Found!"});
+        }
+    }
+    catch(err){
+        console.log(err)
+        return res.status(502).json(err);
+    }
     return res.status(200).json(allEmployees);
 };
 
 //--------- Get employee by ID ---------
 EmployeeController.getEmployeeById = async (req, res, next)=> {
     let id = req.params.id;
+    let foundEmployee
 
-    let foundEmployee =await Employee.findOne({'id':id})
+    try{
+        foundEmployee = await Employee.findOne({'id':id})
 
+        if(!foundEmployee){
+            return res.status(404).json({message: `No Employee Found with ID ${id}`});
+        }
+    }
+    catch(err){
+        console.log(err)
+        return res.status(502).json(err);
+    }
     return res.status(200).json(foundEmployee);
 };
 
 // --------- Add a new employee ---------
 EmployeeController.addEmployee = async (req, res, next)=>{
-    const {
-        id,
-        name,
-        gender,
-        age,
-        state
-    }=req.body;
+    const { id, name, gender, age, state } = req.body;
 
     let employee = new Employee(
         {
-            id, name, state, gender, age
+            id, name, gender, age, state
         }
     )
-    result= await employee.save();
 
-    return res.status(201).send(result);
+    try{
+        await employee.save();
+    }
+    catch(err){
+        console.log(err)
+        return res.status(502).json(err);
+    }
+    return res.status(201).send({message: "Employee added successfully!"});
 };
 
 // --------- Edit employee by ID ---------
 EmployeeController.editEmployee = async (req, res, next)=> {
-    res.send('render edit form');
+    const id = req.params.id;
+    const data = req.body
+
+    Employee.updateOne({'id':id}, {$set: data}, (err, result)=>{
+        if(err){
+            console.log(err)
+            return res.status(502).json(err);
+        }
+        else{
+            return res.status(200).json({message: "Employee details updated successfully!"});
+        }
+    })
 };
 
 // --------- Delete employee by ID ---------
 EmployeeController.deleteEmployee = async (req, res, next)=> {
     const id = req.params.id;
 
-    deletedEmployee = await Employee.findOneAndDelete({id:id})
-    
-    return res.status(201).send(deletedEmployee);
+    try{
+        await Employee.findOneAndDelete({id:id})
+    }
+    catch(err){
+        console.log(err)
+        return res.status(502).json(err);
+    }
+    return res.status(201).send({message: `Employee with ID ${id} deleted successfully!`});
 };
 
 module.exports = EmployeeController;
--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 const employee = require('./routes/Employee'); 
 
 // ------------------- MIDDLEWARE -------------------
+app.use(cors({credentials: true, origin:"http://localhost:3000"}));
 app.use(express.json());
-mongoose.connect('mongodb+srv://sync212121:TDgYSV4jCA1AJIkV@ems.9t1fpmk.mongodb.net/');
 
-app.listen(port, () => {
-    console.log(`App listening at port 3000`);
-});
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://sync212121:TDgYSV4jCA1AJIkV@ems.9t1fpmk.mongodb.net/')
+    .then(()=>{
+        console.log('MongoDB is connected!');
+        app.listen(port, () => {
+            console.log(`App is listening at port ${port}`);
+        });
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 
 // ------------------- ROUTES -------------------
 app.use('/employee', employee); 
